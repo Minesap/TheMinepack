eb2ebf333ccdb314ca656adb7253792328615657
Minesap <toddsayshello@gmail.com>
2017-02-04 10:56:14 -0600

    Add files via upload

---
+++ TheMinepack.cs
@@ -0,0 +1,30 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheMinepack
+{
+	class TheMinepack : Mod
+	{
+		public TheMinepack()
+		{
+			Properties = new ModProperties()
+			{
+				Autoload = true,
+				AutoloadGores = true,
+				AutoloadSounds = true
+			};
+		}
+
+        public override void AddRecipeGroups()
+        {
+            RecipeGroup group = new RecipeGroup(() => Lang.misc[37] + " Gold Bar", new int[] // Any Gold Bar
+            {
+                ItemID.GoldBar,
+                ItemID.PlatinumBar,  
+            });
+            RecipeGroup.RegisterGroup("Gold Bar", group);
+        }
+
+    }
+}
