2bb3c5457b198c1a70f62e84acb35ce785675f95
Kachow911 <torniky@gmail.com>
2017-02-04 09:51:48 -0800

    Update SlimyYoyo.cs

--- Items/Weapons/SlimyYoyo.cs
+++ Items/Weapons/SlimyYoyo.cs
@@ -5,50 +5,32 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Minepack.Items;
 
-namespace TheMinepack.Items.Weapons
+namespace Minepack.Items.Weapons {
+public class SlimyYoyo : ModItem
 {
-    public class SlimyYoyo : ModItem
+    public override void SetDefaults()
     {
-        public override bool Autoload(ref string name, ref string texture, IList<EquipType> equips)
-        {
-            texture = "TheMinepack/Items/Weapons/SlimyYoyo";
-            return true;
-        }
-
-        public override void SetDefaults()
-        {
-            item.CloneDefaults(ItemID.Code1); // Clones Code 1
-
-            // Item Details
-            item.name = "Slimy Yoyo";
-            item.rare = 1;
-            item.value = Item.sellPrice(0, 0, 50, 0);
-
-            // Item Animation & Sounds
-            item.useTime = 22;
-            item.useAnimation = 22;
-            item.useStyle = 5;
-
-            // Item Stats
-            item.melee = true;
-            item.damage = 20;
-            item.knockBack = 1;
-
-            // Item Functionality
-            item.channel = true;
-            item.autoReuse = false;
-            item.shoot = mod.ProjectileType("SlimyYoyoProjectile");
-        }
-
-        public override void AddRecipes()
-        {
-            ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(null, "GelatinousBar", 10); // 10 Gelatinous Bars
-            recipe.AddIngredient(null, "YoyoString", 1); // 1 Yoyo String
-            recipe.AddTile(TileID.Solidifier); // Solidifer
-            recipe.SetResult(this);
-            recipe.AddRecipe();
-        }
+    	item.CloneDefaults(ItemID.Valor);
+        item.name = "Slimy Yoyo";
+        item.damage = 12;
+        item.channel = true;
+        item.melee = true;
+        item.knockBack = 4;
+        item.value = 50000;
+        item.rare = 3;
+        item.autoReuse = false;
+        item.shoot = mod.ProjectileType("SlimyYoyo");
     }
-}
+    
+    public override void AddRecipes()
+	{
+		ModRecipe recipe = new ModRecipe(mod);
+        recipe.AddIngredient(null, "GelatinousBar", 10);
+		recipe.AddIngredient(null, "YoyoString", 1);
+        recipe.AddTile(TileID.Solidifier);
+        recipe.SetResult(this);
+        recipe.AddRecipe();
+	}
+}}
