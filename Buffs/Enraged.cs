7dfcd8475b43f505165e28d1fd2ed2aabaae6837
Minesap <toddsayshello@gmail.com>
2017-02-04 11:00:35 -0600

    Add files via upload

---
+++ Buffs/Enraged.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheMinepack.Items;
+
+namespace TheMinepack.Buffs
+{
+	public class Enraged : ModBuff
+	{
+		public override void SetDefaults()
+		{
+            Main.buffName[this.Type] = "Enraged";
+            Main.buffTip[this.Type] = "The curse strengthens your weapons and weakens your body";
+            Main.debuff[Type] = false;
+            Main.pvpBuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+            longerExpertDebuff = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            // Damage multiplied by 0.2 of weapons base damage
+            player.meleeDamage += 0.2f;
+            player.magicDamage += 0.2f;
+            player.rangedDamage += 0.2f;
+            player.thrownDamage += 0.2f;
+            player.minionDamage += 0.2f;
+
+            // Crit chance increased by 10
+            player.meleeCrit += 10;
+            player.magicCrit += 10;
+            player.rangedCrit += 10;
+            player.thrownCrit += 10;
+
+            // Defense lowered by 5
+            player.statDefense -= 5;
+        }
+    }
+}
---
+++ Buffs/Vulnerable.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheMinepack.Items;
+
+namespace TheMinepack.Buffs
+{
+	public class Vulnerable : ModBuff
+	{
+		public override void SetDefaults()
+		{
+            Main.buffNoTimeDisplay[this.Type] = false;
+            Main.buffName[this.Type] = "Vulnerable";
+            Main.buffTip[this.Type] = "You no longer have invincibility frames";
+            Main.debuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            if (player.immune)
+            {
+                player.immune = false;
+            }
+        }
+    }
+}
