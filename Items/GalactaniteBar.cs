7ab7855cce04b616417c2d85b4a032d76c48cd9d
Minesap <toddsayshello@gmail.com>
2017-02-07 17:22:29 -0600

    Add files via upload

---
+++ Items/GalactaniteBar.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheMinepack.Items
+{
+	public class GalactaniteBar : ModItem
+	{
+		public override void SetDefaults()
+		{
+            // Item Details
+			item.name = "Galactanite Bar";
+            AddTooltip("...");
+            item.width = 10;
+			item.height = 10;
+            item.rare = 8;
+            item.value = Item.sellPrice(0, 0, 20, 0);
+            item.maxStack = 999;
+		}
+
+        public override DrawAnimation GetAnimation()
+        {
+            return new DrawAnimationVertical(7, 6);
+        }
+    }
+}
