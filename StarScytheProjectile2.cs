7265c29831705f6f57338f99f8353418dcf6ed39
Minesap <toddsayshello@gmail.com>
2017-02-09 17:13:23 -0600

    Updated Code + 2nd Projectile has custom sprite

---
+++ StarScytheProjectile2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheMinepack.Projectiles
+{
+    public class StarScytheProjectile2 : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+        	projectile.CloneDefaults(ProjectileID.Starfury);
+            projectile.name = "Small Star";
+            projectile.width = 22;
+            projectile.height = 22;
+			projectile.aiStyle = 5;
+			aiType = 9;
+        }
+    }
+}
